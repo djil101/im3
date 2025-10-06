99cd8deee300428af37bd6dd9a8bc8c43c5692ed
djil101 <damiana.jil@gmail.com>
2025-10-06 10:32:56 +0200

    php

--- etl-boilerplate/extract.php
+++ etl-boilerplate/extract.php
@@ -16,22 +16,23 @@
 
 function fetchWeatherData()
 {
-    $url = "https://api.open-meteo.com/v1/forecast?latitude=46.9481,46.8499,47.3667&longitude=7.4474,9.5329,8.55&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,rain,showers,snowfall,cloud_cover&temperature_unit=fahrenheit&timezone=auto&forecast_days=1";
+    $url = "https://api.nextbike.net/maps/nextbike-live.json?lat=46.85475&lng=9.52649";
 
     // Initialisiert eine cURL-Sitzung
-
+    $ch = curl_init($url);
 
     // Setzt Optionen
-
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Rückgabe des Transfers als String
 
     // Führt die cURL-Sitzung aus und erhält den Inhalt
-
+    $response = curl_exec($ch);
 
     // Schließt die cURL-Sitzung
-
+    curl_close($ch);
+    print_r($response);
 
     // Dekodiert die JSON-Antwort und gibt Daten zurück
-
+   
 }
 
 // Gibt die Daten zurück, wenn dieses Skript eingebunden ist
